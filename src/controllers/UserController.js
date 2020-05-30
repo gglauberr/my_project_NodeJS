72baba8d938f51e734cead452dc7afea53cee6c8
gglauberr <glauberpp16@yahoo.com.br>
2020-05-30 16:01:07 -0300

    CRUD users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,9 +14,49 @@ module.exports = {
     },
 
     async store(req, res){
-        const user = new UserModel(req.body)
+        const { login, password, ativo } = req.body
+        await UserModel
+            .create({ login, password, ativo })
+            .then(response => {
+                return res.status(200).json(response)
+            })
+            .catch(error => {
+                return res.status(500).json(error)
+            })
+    },
+
+    async show(req, res){
+        await UserModel
+            .findByPk(req.params.id)
+            .then(response => {
+                return res.status(200).json(response)
+            })
+            .catch(error => {
+                return res.status(500).json(error)
+            })
+    },
+
+    async update(req, res){
+        const { id } = req.params
+        const { login, password, ativo } = req.body
+        const user = await UserModel.findByPk(id)
+        if(!user) return res.status(400).json({ error: 'Usuário não encontrado' })
         await user
-            .save()
+            .update({ login, password, ativo })
+            .then(response => {
+                return res.status(200).json(response)
+            })
+            .catch(error => {
+                return res.status(500).json(error)
+            })
+    },
+
+    async delete(req, res){
+        const { id } = req.params
+        const user = await UserModel.findByPk(id)
+        if(!user) return res.status(400).json({ error: 'Usuário não encontrado' })
+        await user
+            .destroy()
             .then(response => {
                 return res.status(200).json(response)
             })
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,9 @@ const UserController = require('../controllers/UserController')
 const routes = Router()
 
 routes.get('/', UserController.index)
+routes.get('/:id', UserController.show)
 routes.post('/', UserController.store)
+routes.put('/:id', UserController.update)
+routes.delete('/:id', UserController.delete)
 
 module.exports = routes
